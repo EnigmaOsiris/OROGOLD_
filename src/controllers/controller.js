ecbacad3160c1f5b205cd78f7439b9151a114ec4
Osiris <osiris@meeplab.com>
2020-05-06 03:35:18 -0500

    modal final

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -5,26 +5,47 @@ module.exports = {
     get: (req, res) => {
         res.render('index', { result: {} });
     },
-    showMap:(req,res) =>{
+    showMap: (req, res) => {
         res.render('mapa', { mensaje: 'hola ' });
     },
-    post: async (req,res)=>{
-        let response={}
+    post: (req, res) => {
+        let response = {}
         try {
-            console.log(req.body);            
-            let query=`insert into Reservaciones values(null,'${req.body.nombre}', '${req.body.email}', '${req.body.telefono}')`;
-            conection.query(query);
-            console.log("ok");
-            response.status="success";
-            response.message="Ok";            
+            console.log(req.body);
+            let exists = `select * from Reservaciones where email='${req.body.email}'`;
+            conection.query(exists, function (err, result, fields) {
+                if (err) throw err;
+                console.log(result);
+                if (result && result.length > 0) {
+                    console.log(`error: ${err}`);
+                    response.status = "error";
+                    response.message = err;
+                    res.json({
+                        result: response
+                    });
+                } else {
+                    let query = `insert into Reservaciones values(null,'${req.body.nombre}', '${req.body.email}', '${req.body.telefono}')`;
+                    // await conection.query(query);
+                    conection.query(query, function (err, result) {
+                        if (err) throw err;
+                        console.log("1 record inserted");
+                        console.log("ok");
+                        response.status = "success";
+                        response.message = "Ok";
+                        res.json({
+                            result: response
+                        });
+                    });
+                }
+            });
+
         } catch (error) {
             console.log(`error: ${error}`);
-            response.status="error";
-            response.message=error;            
+            response.status = "error";
+            response.message = error;
+            res.json({
+                result: response
+            });
         }
-        res.render("index", {
-            result:response
-        });
-        
     }
 };
